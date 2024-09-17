edca8bd7ec3addc97683bcf06c3bdbae7998391c
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-09-17 08:18:47 -0500

    Undo commit

--- bookmarkapp/models/database.py
+++ bookmarkapp/models/database.py
@@ -183,9 +183,6 @@ class Database:
         if (password == ''):
             errors.append('Password is required.')
 
-        if (len(errors) > 0):
-            raise ExceptionList(errors)
-
         cursor = cls._get_cursor()
         cursor.execute("""
                         SELECT id, user_name, display_name, password, privilege
